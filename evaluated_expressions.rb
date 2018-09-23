d6ad5edf3de726ee83fadb18bb807cc2bab7e3c0
user <t.rickard7@hotmail.com>
2018-09-23 17:02:14 -0500

    create evaluated_expressions program

---
+++ evaluated_expressions.rb
@@ -0,0 +1,9 @@
+puts 3 + 3 + 3 * 10 == 3 + 3 + 3 * 10
+puts (2 * 4) / 2 - 4 == (2 * 4) / 2 - 4
+puts "hello" * 3 == "hello" "hello" "hello"
+puts "hello" + "3" == "hello3"
+puts !true == false
+puts true && !false
+puts "false" == "false"
+puts false || true
+puts false || true && 3 == 3
