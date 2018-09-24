d1cdbc3b5b37ac849712a5721dbd6ee9595e0de5
user <t.rickard7@hotmail.com>
2018-09-24 01:24:13 -0500

    create hello_method program

---
+++ hello_method.rb
@@ -0,0 +1,7 @@
+
+def hello_method(name)
+  puts "Hello " + name
+end
+
+hello_method("Mitch")
+hello_method("bob")
--- number_incrementor.rb
+++ number_incrementor.rb
@@ -1,6 +1,4 @@
-puts "Enter a number! Please."
-number = gets.chomp.to_i
-
+def increment(number)
 puts number
 puts number +1
 puts number +2
@@ -11,3 +9,6 @@ puts number +6
 puts number +7
 puts number +8
 puts number +9
+end  
+
+increment(42)
