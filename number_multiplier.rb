8ff1821c2adf20a0711f6d5c39c22ba1c055a83c
user <t.rickard7@hotmail.com>
2018-09-23 20:07:36 -0500

    create number_multiplier.rb

---
+++ number_multiplier.rb
@@ -0,0 +1,11 @@
+puts "Please enter a number."
+
+Number_1 = gets.chomp.to_i
+
+puts "Please enter a second number."
+
+Number_2 = gets.chomp.to_i
+
+Number_3 = Number_1 * Number_2
+
+puts "#{Number_1} * #{Number_2} is #{Number_3}. Thank you. :)"
