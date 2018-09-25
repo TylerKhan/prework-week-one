a315930c43049653c891df92028c41db14a4337d
user <t.rickard7@hotmail.com>
2018-09-25 17:35:23 -0500

    create loops.rb

---
+++ loops.rb
@@ -0,0 +1,32 @@
+# 15.times do
+# puts "hello"
+# end
+
+# counter = 0
+
+# while counter < 15
+#  puts "hello"
+#   counter +=1
+# end
+
+# puts "the loop has ended."
+
+# [1,2,3,4,5, "jello"].each do |value|
+#  puts value
+# end
+
+# array = [1,2,3,4,5, "jello"]
+# index = 0
+# array.length.times do
+
+# puts array[index]
+# index += 1
+
+# end
+
+array = [1,2,3,4,5]
+index = 0
+while index < array.length
+  puts array[index]
+  index += 1
+end
